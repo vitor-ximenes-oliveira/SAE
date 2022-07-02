b450d7cbef26c0488f921550fd0b1cf3f2f9b93e
Andre-Oli-2800 <108578615+Andre-Oli-2800@users.noreply.github.com>
2022-07-02 17:05:28 -0300

    Update models.py

--- SAE/website/models.py
+++ SAE/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    id = models.AutoField(primary_key = True)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
+    class Meta:
+        db_table = "aluno"
