a888cc788bf19dac7dd39067a46c09200673369c
Andre-Oli-2800 <108578615+Andre-Oli-2800@users.noreply.github.com>
2022-07-25 14:32:22 -0300

    Update forms.py

--- SAE/website/forms.py
+++ SAE/website/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from website.models import Aluno, Professor
+from website.models import Aluno, Professor, Feedback
 
 class AlunoForm(forms.ModelForm):
     class Meta:
@@ -12,3 +12,8 @@ class ProfessorForm(forms.ModelForm):
         model = Professor
         fields = "__all__"
         widgets = {'pf_senha': forms.PasswordInput()}
+        
+class FeedBackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = "__all__"
