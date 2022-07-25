abe32e3882cc7b3b86d209a8edd38477bd9dc4ab
Andre-Oli-2800 <108578615+Andre-Oli-2800@users.noreply.github.com>
2022-07-25 14:31:13 -0300

    Update models.py

--- SAE/website/models.py
+++ SAE/website/models.py
@@ -22,3 +22,33 @@ class Professor(models.Model):
             return bool(self.isProf)
     class Meta:
         db_table = "Professor"
+
+OPCOES = [
+        ('ruim','Ruim'),
+        ('bom','Bom'),
+        ('médio','Médio'),
+        ('excelente','Excelente'),
+]
+
+class Feedback(models.Model):
+    id = models.AutoField(primary_key=True)
+    pergunta1 = models.CharField(max_length=10,choices=OPCOES, blank=False)
+    justificativa1 = models.CharField(max_length=255,blank = False)
+    pergunta2 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa2 = models.CharField(max_length=255,blank = False)
+    pergunta3 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa3 = models.CharField(max_length=255,blank = False)
+    pergunta4 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa4 = models.CharField(max_length=255,blank = False)
+    pergunta5 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa5 = models.CharField(max_length=255,blank = False)
+    pergunta6 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa6 = models.CharField(max_length=255,blank = False)
+    pergunta7 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa7 = models.CharField(max_length=255,blank = False)
+    pergunta8 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa8 = models.CharField(max_length=255,blank = False)
+    pergunta9 = models.CharField(max_length=10, choices=OPCOES,blank = False)
+    justificativa9 = models.CharField(max_length=255,blank = False)
+    class Meta:
+        db_table = "feedback"        
