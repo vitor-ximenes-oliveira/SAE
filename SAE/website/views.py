7f6ab3d0995600abcdf1c43d321e1c26a1b5dd9c
Andre-Oli-2800 <108578615+Andre-Oli-2800@users.noreply.github.com>
2022-07-25 14:30:16 -0300

    Update views.py

--- SAE/website/views.py
+++ SAE/website/views.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 from django.shortcuts import redirect, render
 from django.contrib.auth.hashers import make_password, check_password
-from website.forms import AlunoForm, ProfessorForm
-from website.models import Aluno, Professor
+from website.forms import AlunoForm, ProfessorForm, FeedBackForm
+from website.models import Aluno, Professor, Feedback
 from django.contrib import messages
+import os
+import pythoncom
+import win32com.client
+from django.http import FileResponse, HttpResponse, HttpResponseRedirect
+from SAE import settings
+from SAE.settings import BASE_DIR, MEDIA_ROOT
+from django.db import DataError, IntegrityError
 
 def cadastro(request):
     form = AlunoForm(request.POST)
@@ -58,6 +65,76 @@ def login_user(request):
                     messages.info(request, "Nome/senha inválido(s)")
             return render(request,"login.html")
 
+def visualizar(request,arquivo):
+    extensoes = [".pdf", ".txt", ".png", ".jpg", ".gif", ".bmp",".mp3"]
+    if arquivo.endswith(tuple(extensoes)):
+        diretorio_arquivo = os.path.join(settings.MEDIA_ROOT, arquivo)
+        arquivo = open(diretorio_arquivo, 'rb') 
+        abrir_Arquivo = FileResponse(arquivo)
+        return abrir_Arquivo
+    elif arquivo.endswith('.xlsx'):
+        diretorio_arquivo = os.path.join(settings.MEDIA_ROOT, arquivo)
+        excel = win32com.client.Dispatch('Excel.Application',pythoncom.CoInitialize())
+        excel.Visible = True
+        abrir_excel = excel.Workbooks.Open(diretorio_arquivo, None,True)
+        return HttpResponseRedirect("/atividades") 
+    elif arquivo.endswith('.docx'): 
+        diretorio_arquivo = os.path.join(settings.MEDIA_ROOT, arquivo)
+        word=win32com.client.Dispatch("Word.Application",pythoncom.CoInitialize())
+        word.Visible = True
+        abrir_excel = word.Documents.Open(diretorio_arquivo, None,True)
+        return HttpResponseRedirect("/atividades") 
+    elif arquivo.endswith('.pptx'):
+        diretorio_arquivo = os.path.join(settings.MEDIA_ROOT, arquivo)
+        PowerPoint = win32com.client.Dispatch("Powerpoint.Application",pythoncom.CoInitialize())
+        PowerPoint.Visible = True
+        Abrir_PowerPoint = PowerPoint.Presentations.Open(diretorio_arquivo, None,True)
+        return HttpResponseRedirect("/atividades") 
+    else:
+        diretorio_arquivo = os.path.join(settings.MEDIA_ROOT, arquivo)
+        os.system(diretorio_arquivo)    
+        return HttpResponseRedirect("/atividades") 
+    
+def baixar(request, arquivo):
+    if arquivo != '':
+        diretorio_arquivo = (os.path.join(settings.MEDIA_ROOT, arquivo))
+        diretorio = open(diretorio_arquivo,'rb')
+        download_arquivo = HttpResponse(diretorio ,content_type="aplicacao/arquivo")
+        download_arquivo ['Content-Disposition'] = "attachment; nome_arquivo=" + arquivo
+        return download_arquivo
+    else:
+        return render(request ,'atividades.html')
+
+def feedback(request):
+    form = FeedBackForm(request.POST)
+    if request.method == 'POST':
+        try:
+            justificativa1 = request.POST.get("Justificativa1")
+            justificativa2 = request.POST.get("Justificativa2")
+            justificativa3 = request.POST.get("Justificativa3")             
+            justificativa4 = request.POST.get("Justificativa4")             
+            justificativa5 = request.POST.get("Justificativa5")             
+            justificativa6 = request.POST.get("Justificativa6")             
+            justificativa7 = request.POST.get("Justificativa7")             
+            justificativa8 = request.POST.get("Justificativa8")             
+            justificativa9 = request.POST.get("Justificativa9")
+            pergunta1 = request.POST.get("btn-radio")
+            pergunta2 = request.POST.get("btn-radio2")
+            pergunta3 = request.POST.get("btn-radio3")
+            pergunta4 = request.POST.get("btn-radio4")
+            pergunta5 = request.POST.get("btn-radio5")
+            pergunta6 = request.POST.get("btn-radio6")
+            pergunta7 = request.POST.get("btn-radio7")
+            pergunta8 = request.POST.get("btn-radio8")
+            pergunta9 = request.POST.get("btn-radio9")
+            feedback = Feedback.objects.create(pergunta1=pergunta1,justificativa1=justificativa1,pergunta2=pergunta2,justificativa2=justificativa2,pergunta3=pergunta3,justificativa3=justificativa3,pergunta4=pergunta4,justificativa4=justificativa4,pergunta5=pergunta5,justificativa5=justificativa5,pergunta6=pergunta6,justificativa6=justificativa6,pergunta7=pergunta7,justificativa7=justificativa7,pergunta8=pergunta8,justificativa8=justificativa8,pergunta9=pergunta9,justificativa9=justificativa9)
+            feedback.save() 
+            messages.success(request,"Feedback enviado com sucesso")           
+            return redirect("/feedback")
+        except IntegrityError:
+            messages.error(request, "Preencha todos os campos")
+    return render(request, 'feedback.html', {'form': form})
+    
 def pagina_aluno(request):
     form = AlunoForm(request.POST)
     return render(request,"pagina_aluno.html")
