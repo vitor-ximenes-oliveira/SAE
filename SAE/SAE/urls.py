28a55e2310cc2d768f1b18523670260d37f8bf1f
Andre-Oli-2800 <108578615+Andre-Oli-2800@users.noreply.github.com>
2022-09-06 14:29:43 -0300

    Update urls.py
    Adicionado a importação da url que estava ausente

--- SAE/SAE/urls.py
+++ SAE/SAE/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from website import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
