93bb426729c14907bb431645e092b0cf44fcf56c
Andre-Oli-2800 <andreoli8000@gmail.com>
2022-09-18 17:33:49 -0300

    Mudança de nome de atributos na views e retirada de código desnecessário

--- SAE/website/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from website.models import Professor,Turmas
-
-class UsuarioAdmin(admin.ModelAdmin):
-    exclude = ('Nome','isProf')
-admin.site.register(Professor,UsuarioAdmin)
